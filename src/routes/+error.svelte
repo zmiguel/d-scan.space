c5ff1d11bc72b6140bec5ff856e520486a1cc2f1
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2024-03-19 00:09:26 +0100

    first

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Button } from 'flowbite-svelte';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="text-center">
+  <h1 class="text-3xl font-bold text-red-500">Oops! An error {$page.status} occurred.</h1>
+	<div class="mt-4 mb-4 dark:text-white">{$page.error?.message}</div>
+  <Button color="primary" on:click={() => goto('/')}>Go Home</Button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Textarea, Label, Button } from 'flowbite-svelte';
+	import { Toggle } from 'flowbite-svelte';
+</script>
+
+<div class="content-center">
+	<Label for="textarea-id" class="mb-2">Paste <span class="text-primary-700 dark:text-primary-400">Local</span> or <span class="text-primary-700 dark:text-primary-400">Directional Scan</span></Label>
+	<Textarea id="textarea-id" placeholder="Paste your data" rows="16" name="message" />
+	<Toggle class="mt-2" checked={false}>Make this Scan public on the site.</Toggle>
+	<Button class="mt-4 w-full" color="primary">Process</Button>
+</div>
