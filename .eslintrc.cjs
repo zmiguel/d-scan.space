dfa39fcd469b9cca7d09f3f847442e39370471a8
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2024-11-12 19:49:12 +0100

    figure out if local or directional

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,18 @@ module.exports = {
 		ecmaVersion: 2020,
 		extraFileExtensions: ['.svelte']
 	},
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx'],
+			parser: '@typescript-eslint/parser',
+			plugins: ['@typescript-eslint'],
+			extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended']
+		},
+		{
+			files: ['**.js'],
+			parser: 'espree' // Default parser for JavaScript files
+		}
+	],
 	env: {
 		browser: true,
 		es2017: true,
--- src/routes/scan/+page.server.js
+++ src/routes/scan/+page.server.js
@@ -7,15 +7,39 @@ import { redirect } from '@sveltejs/kit';
 export const actions = {
 	create: async ({ request, platform }) => {
 		const data = await request.formData();
-		const content = data.get('scan_content');
+		const content = /** @type {(string | null)} */ (data.get('scan_content'));
 		const is_public = data.has('is_public');
+		const db = drizzle(platform?.env.DB);
+
+		if (!content) {
+			return { status: 400, body: 'No scan content provided' };
+		}
+
+		const lines = content.split('\n');
+
+		// Figure out if local or directional scan
+		//  - Directional scans start with numbers and have 3 tabs per line
+		const isDirectional = lines.every((line) => {
+			const parts = line.split('\t');
+			return parts.length === 4 && !isNaN(parts[0]);
+		});
+
+		// LOCAL SCAN
+		if (!isDirectional) {
+			// 2. Send lines to processor function
+			//
+			// 3. Save the scan in KV Store
+			//
+			// 4. Add data to DB
+		} else {
+			// DIRECTIONAL SCAN
+			//
+			// TBD.
+		}
 
 		const uid = new ShortUniqueId();
-
-		let db = drizzle(platform.env.DB);
-
-		let scanGroupId = uid.randomUUID(8);
-		let scanId = uid.randomUUID(12);
+		const scanGroupId = uid.randomUUID(8);
+		const scanId = uid.randomUUID(12);
 
 		await db.insert(scanGroups).values({
 			id: scanGroupId,
