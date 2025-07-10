156b03a3fdf476e980c1bc2ce404406dfb74b549
José Valdiviesso <zmiguel.alves@gmail.com>
2025-07-10 11:51:32 +0200

    log database url on startup

--- src/lib/database/client.js
+++ src/lib/database/client.js
@@ -1,4 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { DATABASE_URL } from '$env/static/private';
 
+console.log("Using database:", DATABASE_URL);
+
 export const db = drizzle(DATABASE_URL);
