db18fda07fa18f6073d62a447706cdc6a2689990
José Valdiviesso <zmiguel.alves@gmail.com>
2025-07-10 12:06:23 +0200

    env

--- src/lib/database/client.js
+++ src/lib/database/client.js
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { DATABASE_URL } from '$env/dynamic/private';
+import { env } from '$env/dynamic/private';
 
-export const db = drizzle(DATABASE_URL);
+export const db = drizzle(env.DATABASE_URL);
 
 await migrate(db);
