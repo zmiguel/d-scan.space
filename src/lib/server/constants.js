4112f4b814c113cfb290f059586bc0139af65dd4
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2025-07-24 14:56:57 +0200

    use correct way

--- src/lib/server/constants.js
+++ src/lib/server/constants.js
@@ -1,5 +1,5 @@
+import { version } from "../../../package.json"
 import { env } from '$env/dynamic/private';
-const pkg = require("../../../package.json")
 
 export const DOOMHEIM_ID = 1000001;
-export const USER_AGENT = `D-Scan.Space/${pkg.version} (${env.NODE_ENV ? '' : env.NODE_ENV + ";"}${env.AGENT ? '' : " " + env.AGENT})`
+export const USER_AGENT = `D-Scan.Space/${version} (${env.NODE_ENV ? '' : env.NODE_ENV + ";"}${env.AGENT ? '' : " " + env.AGENT})`
