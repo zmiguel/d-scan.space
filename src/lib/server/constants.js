13dccffbbaa992d8fb0dfbed486f09a4383e4f51
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2025-07-24 14:55:55 +0200

    fix package

--- src/lib/server/constants.js
+++ src/lib/server/constants.js
@@ -1,5 +1,5 @@
-import pkg from "../../../package.json"
 import { env } from '$env/dynamic/private';
+const pkg = require("../../../package.json")
 
 export const DOOMHEIM_ID = 1000001;
 export const USER_AGENT = `D-Scan.Space/${pkg.version} (${env.NODE_ENV ? '' : env.NODE_ENV + ";"}${env.AGENT ? '' : " " + env.AGENT})`
