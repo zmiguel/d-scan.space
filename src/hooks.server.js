af7dfacad19754bb8cf53264a87b746360604c47
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2025-07-24 15:12:08 +0200

    remove cron temp

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,5 +1,5 @@
-import cron from 'node-cron';
-import { updateDynamicData } from '$lib/cron/dynamic';
+//import cron from 'node-cron';
+// import { updateDynamicData } from '$lib/cron/dynamic';
 import { USER_AGENT } from '$lib/server/constants';
 
 /** @type {import('@sveltejs/kit').ServerInit} */
@@ -7,14 +7,14 @@ export async function init() {
     console.info("Current User-Agent: " + USER_AGENT);
 
     // update dynamic data once per day, after downtime
-    cron.schedule('30 11 * * *', async () => {
-        try {
-            console.log('[CRON] Updating dynamic data...');
-            await updateDynamicData();
-            console.log('[CRON] Dynamic data updated successfully');
-        } catch (error) {
-            console.error('[CRON] Failed to update dynamic data:', error);
-        }
-    });
+    // cron.schedule('30 11 * * *', async () => {
+    //     try {
+    //         console.log('[CRON] Updating dynamic data...');
+    //         await updateDynamicData();
+    //         console.log('[CRON] Dynamic data updated successfully');
+    //     } catch (error) {
+    //         console.error('[CRON] Failed to update dynamic data:', error);
+    //     }
+    // });
 
 }
