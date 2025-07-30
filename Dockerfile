795d2ceb7aa0865b412a3d91851776b649f3a424
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2025-07-30 10:25:40 +0100

    fix docker

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ COPY --from=builder /app/build build/
 COPY --from=builder /app/node_modules node_modules/
 COPY package.json .
 COPY instrumentation.mjs .
+COPY src/lib/logger.js src/lib/logger.js
 COPY drizzle drizzle/
 EXPOSE 4173
 ENV NODE_ENV=production
--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,12 +1,11 @@
 import pino from 'pino';
 
-const formatters = {
-    level (label, number) {
-        return { level: label, priority: number };
-    }
-}
-
 export default pino({
 	level: process.env.LOG_LEVEL || 'info',
-	formatters: formatters
+	formatters: {
+		level (label, number) {
+			return { level: label, priority: number };
+		}
+	},
+    timestamp: pino.stdTimeFunctions.isoTime,
 });
