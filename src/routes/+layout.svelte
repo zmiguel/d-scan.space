a971f34ca33f6efdd4f51a5ef3d1d905beca1101
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2025-08-01 18:30:51 +0100

    add partner branding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import '../app.css';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
+	import { asset } from '$app/paths';
 	let { children } = $props();
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, FooterBrand } from 'flowbite-svelte';
 	import { Footer, FooterCopyright, FooterLink, FooterLinkGroup } from 'flowbite-svelte';
 	import { DarkMode } from 'flowbite-svelte';
-	const activeUrl = $derived($page.url.pathname);
+	const activeUrl = $derived(page.url.pathname);
 </script>
 
 <div class="relative px-8">
@@ -42,15 +43,18 @@
 	<div class="mt-16 mb-8 text-gray-900 dark:text-gray-300">
 		{@render children()}
 	</div>
-	<Footer class="pb-4">
+	<Footer class="logo">
 		<FooterCopyright href="/" by="D-Scan" />
-		<FooterLinkGroup
+		<div class="sm:flex sm:items-center sm:justify-between">
+			<FooterLinkGroup
 			ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
-		>
+			>
 			<FooterLink href="/about">About</FooterLink>
 			<FooterLink href="/ccp">CCP Copyright Notice</FooterLink>
 			<FooterLink href="/contact">Contact</FooterLink>
 			<FooterLink aClass="text-blue-500 dark:text-blue-400 hover:underline" href="https://store.eveonline.com/" target="_blank">Use code <span class="font-bold italic text-red-500 dark:text-red-400">OXED</span> on the EVE Online Store</FooterLink>
 		</FooterLinkGroup>
+		<FooterBrand imgClass="me-4 w-auto h-16" href="https://www.eveonline.com/partners" target="_blank" src={asset('/PartnerBadge2.png')} alt="Partner Link" />
+		</div>
 	</Footer>
 </div>
