19b0925f4fa905b89c9d39a9f13e162cab2483ee
José Valdiviesso <zmiguel.alves@gmail.com>
2025-08-27 17:23:30 +0200

    fix migrations again

--- drizzle/0002_minor_chimera.sql
+++ drizzle/0002_minor_chimera.sql
@@ -1,2 +1,9 @@
-ALTER TABLE "sde_data" ALTER COLUMN "id" SET DATA TYPE integer;--> statement-breakpoint
-ALTER TABLE "sde_data" ALTER COLUMN "id" ADD GENERATED ALWAYS AS IDENTITY (sequence name "sde_data_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1);
+DROP TABLE IF EXISTS "sde_data";--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "sde_data" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "sde_data_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"install_date" timestamp DEFAULT now() NOT NULL,
+	"fsd_checksum" text NOT NULL,
+	"bsd_checksum" text NOT NULL,
+	"universe_checksum" text NOT NULL,
+	"success" boolean DEFAULT true NOT NULL
+);
