df4a20c203563b6e4460d31ed955c5be3f5488f7
José Valdiviesso <zmiguel.alves@gmail.com>
2025-11-21 11:00:33 +0100

    improve logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,7 +1,13 @@
 import pino from 'pino';
 
+const isUpdater = typeof process !== 'undefined' && process.argv?.[1]?.includes('updater');
+const appName = isUpdater ? 'UPDATER' : 'MAIN';
+
 export default pino({
 	level: process.env.LOG_LEVEL || 'info',
+	mixin() {
+		return { app: appName };
+	},
 	formatters: {
 		level(label, number) {
 			return { level: label, priority: number };
